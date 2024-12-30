16298b21f88e8b9149e2b45942a586cb26590c56
elorm-11 <elormkovey@gmail.com>
2024-12-30 19:55:59 +0000

    4 commit

--- main2.py
+++ main2.py
@@ -1,27 +1,27 @@
-class Ghana():
-    def capital(self):
-        print("Greater Accra is the capital of Ghana.")
+class  Range():
+    def price(self):
+        print("i cost From $107,400 MSRP")
 
-    def language(self):
-        print("English is the most widely spoken language of Ghana.")
-
-    def type(self):
-        print("Ghana is a developing country.")
-
-class USA():
-    def capital(self):
-        print("washington, D.C, is the capital of USA.")    
-
-    def language(self):
-          print("English is the primary language of USA.")
+    def size(self):
+        print("i'm  5052 mm length and 	2209 mm wigth ")
 
     def type(self):
-        print("USA is a developing country.")
+        print("i'm a  Luxury SUVs and 4x4 Vehicle.")
 
-obj_gha = Ghana() 
-obj_usa = USA()
+class bugatti():
+    def price(self):
+        print("i cost starts at $2,990,000 and goes up to $3,900,000 depending on the trim and options.")    
 
-for country in (obj_gha, obj_usa):
-    country.capital()
-    country.language()
+    def size(self):
+          print("i'm around the length of 14'10.9” (454 cm), overall width of 6'8.2” (204 cm), and height of 3'11.7” (121 cm).")
+
+    def type(self):
+        print("i'm a sport car")
+
+obj_ran = range() 
+obj_bug = bugatti()
+
+for country in (obj_ran, obj_bug):
+    country.price()
+    country.size()
     country.type()
